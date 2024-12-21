373de7407cce56a27cf281d5dc09fe4440996f21
code-lova <ebinuga@hotmail.com>
2024-12-21 07:41:09 +0100

    cookies mixup in live api

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -11,7 +11,7 @@ const getEnv = (key: string, defaultValue?: string): string => {
 
 
 export const MONGO_URI = getEnv("MONGO_URI");
-export const NODE_ENV = getEnv("NODE_ENV", "development");
+export const NODE_ENV = getEnv("NODE_ENV", "production");
 export const PORT = getEnv("PORT", "4004");
 export const APP_ORIGIN = getEnv("APP_ORIGIN");
 export const JWT_SECRET = getEnv("JWT_SECRET");
